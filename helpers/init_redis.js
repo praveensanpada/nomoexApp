e9dfce84ea421d0c57369d304586fa104e677922
Praveen Sanpada <60234841+praveensanpada@users.noreply.github.com>
2022-06-07 14:05:30 +0530

    Update init_redis.js

--- helpers/init_redis.js
+++ helpers/init_redis.js
@@ -2,7 +2,7 @@ const redis = require('redis')
 
 const client = redis.createClient({
   port: 6379,
-  host: '127.0.0.1',
+  host: 'nodedb-001.hhtihc.0001.usw2.cache.amazonaws.com',
   // password: "password"
 })
 
